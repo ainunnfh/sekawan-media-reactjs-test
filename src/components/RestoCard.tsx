13598ec2020b6afe9a686f5c169fadbd3057d519
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-21 21:02:24 +0700

    🆙update (resto card): create props

--- src/components/RestoCard.tsx
+++ src/components/RestoCard.tsx
@@ -1,20 +1,27 @@
 import React from "react";
-import Image from "next/image";
 
-const RestoCard = () => {
+interface IProps {
+    image?:string;
+    title: string;
+    starRating: string;
+    categories: string;
+    isOpen: string;
+}
+
+const RestoCard = (props: IProps) => {
   return (
     <div className="mx-3 p-4 border border-separate">
       <div className="">
         <div>
-          <img src="https://picsum.photos/150/120?grayscale" alt="" />
+          <img src="https://picsum.photos/200/120?grayscale" alt="" />
         </div>
 
-        <div className="mt-2">title</div>
-        <div>star rating</div>
+        <div className="mt-2">{props.title}</div>
+        <div>{props.starRating}</div>
 
-        <div className="flex gap-3">
-          <p>categories</p>
-          <p>isOpen</p>
+        <div className="flex justify-between">
+          <p>{props.categories}</p>
+          <p className="text-sm">{props.isOpen}</p>
         </div>
         <button className="w-full p-1 bg-blue-950 font-sm hover:bg-blue-900 text-white font-thin rounded-sm">
           LEARN MORE
