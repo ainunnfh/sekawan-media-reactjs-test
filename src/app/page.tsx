25fcc78988fcb7a210679f51cbdb4f2f3e7f7668
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-21 20:19:50 +0700

    🆙update (page): usage filter component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import Filter from "@/components/Filter";
 import Header from "@/components/Header";
 import Image from "next/image";
 
 export default function Home() {
   return (
+    <>
     <Header/>
+    <Filter/>
+    </>
   );
 }
