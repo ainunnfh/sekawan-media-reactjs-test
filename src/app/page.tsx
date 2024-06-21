2102adb09254a54ddea034163aa1b122591b80d4
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-21 21:02:45 +0700

    🆙update (page): passing resto card props

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Filter from "@/components/Filter";
 import Header from "@/components/Header";
+import RestoCard from "@/components/RestoCard";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,14 @@ export default function Home() {
     <>
     <Header/>
     <Filter/>
+
+    <div className="flex gap-2">
+
+    <RestoCard title={"Tittle 1"} starRating={"⭐⭐⭐⭐⭐"} categories={"Thai"} isOpen={"OPEN NOW"} />
+    <RestoCard title={"Tittle 2"} starRating={"⭐⭐⭐"} categories={"Seafood"} isOpen={"CLOSED"}/>
+    <RestoCard title={"Tittle 3"} starRating={"⭐⭐⭐⭐"} categories={"Japanese"} isOpen={"OPEN NOW"}/>
+    <RestoCard title={"Tittle 4"} starRating={"⭐⭐⭐⭐"} categories={"Italian"} isOpen={"OPEN NOW"}/>
+    </div>
     </>
   );
 }
