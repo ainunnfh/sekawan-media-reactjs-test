67dc3370f984088d1373253404a7a970f1ff3eed
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-21 20:51:35 +0700

    ⭐new: create restaurant card component

---
+++ src/components/RestoCard.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+
+const RestoCard = () => {
+  return (
+    <div className="mx-3 p-4 border border-separate">
+      <div className="">
+        <div>
+          <img src="https://picsum.photos/150/120?grayscale" alt="" />
+        </div>
+
+        <div className="mt-2">title</div>
+        <div>star rating</div>
+
+        <div className="flex gap-3">
+          <p>categories</p>
+          <p>isOpen</p>
+        </div>
+        <button className=" bg-blue-950 font-sm hover:bg-blue-900 text-white font-thin rounded-sm">
+          LEARN MORE
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default RestoCard;
