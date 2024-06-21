d8528b346e509d3cdede2c92d8cc0bd1fa691b41
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-21 20:09:46 +0700

    ⭐new: create header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Header = () => {
+  return <div className="container mx-4 p-4">
+    <div className="text-3xl font-light ">
+        Restaurants
+    </div>
+
+    <div className="mt-2 font-light">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+    </div>
+  </div>;
+};
+
+export default Header;
