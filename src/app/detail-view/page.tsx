3a56f6c0a9d30817c2b9361d2efd54f53a929b7f
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-22 19:04:04 +0700

    ⭐new: routing for detail view page

---
+++ src/app/detail-view/page.tsx
@@ -0,0 +1,8 @@
+import DetailViewComponent from "@/components/DetailViewComponent";
+import React from "react";
+
+const DetailViewPage = () => {
+  return <div><DetailViewComponent/></div>;
+};
+
+export default DetailViewPage;
