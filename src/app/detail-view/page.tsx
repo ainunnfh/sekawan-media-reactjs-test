0c19e5573110eb0b50e35716bed5bc797cdf2420
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-24 16:10:00 +0700

    remove unused file

--- src/app/detail-view/page.tsx
+++
@@ -1,8 +0,0 @@
-import DetailViewComponent from "@/components/DetailViewComponent";
-import React from "react";
-
-const DetailViewPage = () => {
-  return <div><DetailViewComponent/></div>;
-};
-
-export default DetailViewPage;
