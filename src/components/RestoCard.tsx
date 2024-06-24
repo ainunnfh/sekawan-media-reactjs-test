c900b672bd2d2483fc30f8515e4ef11418dc906e
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-06-24 19:22:53 +0700

    🆙update (RestoCard): navigation to detail view page

--- src/components/RestoCard.tsx
+++ src/components/RestoCard.tsx
@@ -2,11 +2,12 @@ import React from "react";
 import StarRating from "./StarRating";
 
 interface IProps {
-    image?:string;
-    title: string;
-    categories: string;
-    isOpen: string;
-    price: string;
+  id: number;
+  image?: string;
+  title: string;
+  categories: string;
+  isOpen: string;
+  price: string;
 }
 
 const RestoCard = (props: IProps) => {
@@ -18,15 +19,20 @@ const RestoCard = (props: IProps) => {
         </div>
 
         <div className="mt-2">{props.title}</div>
-        <div><StarRating/></div>
+        <div>
+          <StarRating />
+        </div>
 
         <div className="flex justify-between">
-          <p>{props.categories}<span className="text-sm"> {props.price}</span></p>
+          <p>
+            {props.categories}
+            <span className="text-sm"> {props.price}</span>
+          </p>
           <p className="text-sm">{props.isOpen}</p>
         </div>
-        <button className="w-full p-1 mt-3 bg-blue-950 font-sm hover:bg-blue-900 text-white font-thin rounded-sm">
+        <a href={`/detail-view/${props.id}`} className="w-full p-1 mt-3 bg-blue-950 font-sm hover:bg-blue-900 text-white font-thin rounded-sm">
           LEARN MORE
-        </button>
+        </a>
       </div>
     </div>
   );
