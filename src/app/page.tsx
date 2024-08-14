b5e95e9c1b513a1f8d31f3a2735b2b5ca76cfc52
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-08-14 22:24:25 +0700

    💡try (page-app): try fetch resto data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
 import Filter from "@/components/Filter";
 import Header from "@/components/Header";
 import RestoCard from "@/components/RestoCard";
-import Image from "next/image";
 import { restoData } from "./restoData";
 
+interface IRestaurant {
+  id: number;
+  name: string;
+  categories: string;
+  price: string;
+  isOpen: string;
+}
+
 export default async function Home() {
+  // const [restoData, setrestoData] = useState<IRestaurant[]>([]);
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await fetch("http://localhost:4000/restaurants");
+  //       const data = await response.json();
+  //       setrestoData(data);
+  //     } catch (error) {
+  //       console.error("Error fetching tickets:", error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+  // console.log(restoData);
+
   return (
     <>
       <Header />
