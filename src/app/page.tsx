631da9062393a1d684447e250114b0711f6d8279
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-09-10 22:30:42 +0700

    🆙update (page): filter by price

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,9 +8,15 @@ import { useState } from "react";
 
 export default function Home() {
   const [selectedFilter, setSelectedFilter] = useState<string>("ALL");
+  const [filterByPrice, setfilterByPrice] = useState<string>("Price");
+  // console.log(selectedFilter);
   const filteredData = restoData.filter((resto) => {
     if (selectedFilter === "ALL") return true;
     return resto.isOpen === selectedFilter;
+  });
+  const filteredPrice = filteredData.filter((resto) => {
+    if (filterByPrice === "Price") return true;
+    return resto.price === filterByPrice;
   });
 
   const handleFilterChange = (value: string) => {
@@ -22,10 +28,12 @@ export default function Home() {
       <Filter
         selectedFilter={selectedFilter}
         setSelectedFilter={handleFilterChange}
+        selectByPrice={filterByPrice}
+        setSelectedPrice={setfilterByPrice}
       />
 
       <div className="flex gap-2 flex-wrap">
-        {filteredData.map((resto) => (
+        {filteredPrice.map((resto) => (
           <RestoCard
             id={resto.id}
             key={resto.name}
