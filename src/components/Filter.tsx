7fb623db8999c73eaf7cc5d2154b1bcd9598d6a4
Ainun Nafiah <ainunnafiah354@gmail.com>
2024-09-10 22:30:22 +0700

    🆙update (filter): create filter by price

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -4,12 +4,18 @@ import { IRestoData, restoData } from "@/app/restoData";
 interface IFilterProps {
   selectedFilter: string;
   setSelectedFilter: (value: string) => void;
+  selectByPrice?: string;
+  setSelectedPrice?: (value: string) => void;
 }
 const Filter: React.FC<IFilterProps> = ({
   selectedFilter,
   setSelectedFilter,
+  selectByPrice,
+  setSelectedPrice,
 }) => {
   const [filteredData, setFilteredData] = useState<IRestoData[]>(restoData);
+  const [filteredPrice, setFilteredPrice] = useState<IRestoData[]>(restoData);
+  // console.log(filteredPrice);
   const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const value = event.target.value;
     setSelectedFilter(value);
@@ -22,17 +28,39 @@ const Filter: React.FC<IFilterProps> = ({
       setFilteredData(restoData);
     }
   };
+
+  const handlePriceChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const value = event.target.value;
+    setSelectedPrice?.(value);
+
+    if (value === "$") {
+      setFilteredPrice(restoData.filter((resto) => resto.price === "$"));
+    } else if (value === "$$") {
+      setFilteredPrice(restoData.filter((resto) => resto.price === "$$"));
+    } else if (value === "$$$") {
+      setFilteredPrice(restoData.filter((resto) => resto.price === "$$$"));
+    } else {
+      setFilteredPrice(restoData);
+    }
+  };
+
   return (
     <div className="mx-3 p-4">
       <div className="flex gap-3">
         Filter By:
-        <div className="flex gap-3 ">
+        <div className="flex gap-3 bg-red-900">
           <select value={selectedFilter} onChange={handleFilterChange}>
             <option value="ALL">All</option>
             <option value="OPEN NOW">Open Now</option>
             <option value="CLOSED">Closed</option>
           </select>
         </div>
+        <select value={selectByPrice} onChange={handlePriceChange}>
+          <option value="price">Price</option>
+          <option value="$">$</option>
+          <option value="$$">$$</option>
+          <option value="$$$">$$$</option>
+        </select>
       </div>
     </div>
   );
